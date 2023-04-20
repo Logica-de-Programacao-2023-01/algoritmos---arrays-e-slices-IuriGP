a9f9c28bf7b5406b9a42850f99f4493094b72b1b
IuriGP <iuri.gp@sempreceub.com>
2023-04-20 17:35:39 -0300

    .

--- Algoritmos Arrays e Slices-18inc.go
+++ Algoritmos Arrays e Slices-18inc.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var x int
-	var primos[int]
+	var n int
+	var primos [int]
 	fmt.print(n)
+	fmt.scan(&n)
+
+	for i:=2,i<=i=n, i++{
+		primo:=true
+		for x:=2;x<i;x++{
+			if i%x==0{primos=falser}
+			break
+		}
+	}
 }
